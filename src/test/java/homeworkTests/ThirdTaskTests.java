90f1fe9e3a718e143cb21d3f020aa1e683088f6f
SergeyYashin <yashinsergey125@gmail.com>
2019-04-16 21:49:32 +0300

    Third task is done

--- src/test/java/homeworkTests/ThirdTaskTests.java
+++ src/test/java/homeworkTests/ThirdTaskTests.java
@@ -1,4 +1,37 @@
 package homeworkTests;
 
+import homework6.ThirdTask;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ThirdTaskTests {
+    ThirdTask tt;
+
+    @Before
+    public void init(){
+        tt = new ThirdTask();
+    }
+
+    @Test
+    public void lookingForNumbersTest1(){
+        Assert.assertTrue(tt.lookingForNumbers(1,2,3,5));
+    }
+
+    @Test
+    public void lookingForNumbersTest2(){
+        Assert.assertTrue(tt.lookingForNumbers(2,3,5,4));
+    }
+
+    @Test
+    public void lookingForNumbersTest3(){
+        Assert.assertFalse(tt.lookingForNumbers(2,3,5,6,9,8,7));
+    }
+
+    @Test
+    public void lookingForNumbersTest4(){
+        int[] array = {254,52,36,98};
+        Assert.assertFalse(tt.lookingForNumbers(array));
+    }
+
 }
